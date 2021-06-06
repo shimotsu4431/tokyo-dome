734b48d256e74ad6a5e6dc5dca7695d413cfef82
shimotsu4431 <shimotsu13@gmail.com>
2021-06-06 09:07:18 +0900

    fix result

--- pages/result.tsx
+++ pages/result.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React, { useContext } from 'react'
 import { Layout } from '../components/Layout'
 import { globalStoreContext } from '../store/GlobalStore'
+import prefData from '../lib/pref'
 
 export const Result: React.FC = () => {
   const { globalStore } = useContext(globalStoreContext)
@@ -17,6 +18,7 @@ export const Result: React.FC = () => {
       <Layout>
         <div>
           <p>{globalStore.searchNumber}</p>
+          <p>{prefData[globalStore.searchPref]}</p>
         </div>
       </Layout>
     </>
