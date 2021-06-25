490f537b869b14588d7a80e1b2179ad88ba00549
shimotsu4431 <shimotsu13@gmail.com>
2021-06-25 09:13:54 +0900

    setup test

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,23 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
-    '^.\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  // see https://github.com/zeit/next.js/issues/8663
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // https://github.com/zeit/next.js/issues/8663#issue-490553899
   globals: {
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.jest.json',
     },
   },
-  // see https://stackoverflow.com/questions/50863312/jest-gives-cannot-find-module-when-importing-components-with-absolute-paths
-  moduleDirectories: ['node_modules', '<rootDir>'],
+  moduleDirectories: ['node_modules', __dirname],
+  moduleNameMapper: {
+    '\\.module\\.scss$': 'identity-obj-proxy',
+  },
 }
