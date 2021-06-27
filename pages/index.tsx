3719862b28795a73896a6c860c2a196e7d4c83ad
shimotsu4431 <shimotsu13@gmail.com>
2021-06-27 09:52:13 +0900

    fix path

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import React, { useContext, useEffect } from 'react'
 import { FormContainer } from '../components/FormContainer'
 import { Layout } from '../components/Layout'
 import { globalStoreContext } from '../store/GlobalStore'
-import styles from '../styles/Pages/Home.module.scss'
+import styles from '../styles/pages/Home.module.scss'
 
 export const Home: React.FC = () => {
   const { globalDispatch } = useContext(globalStoreContext)
